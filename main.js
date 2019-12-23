8871fc34376b68f1de3f88a73292dc632208f432
UmbertoV <umberto.ottone@gmail.com>
2019-12-23 20:53:02 +0100

    Aggiunta chiamate telefilm

--- main.js
+++ main.js
@@ -2,14 +2,18 @@ $(document).ready(function() {
 
     var templateHtml = $("#template").html();
     var templateFunction = Handlebars.compile(templateHtml);
+    var templateHtmlSerie = $("#templateSerietv").html();
+    var templateFunctionSerie = Handlebars.compile(templateHtmlSerie);
 
     $(".bottone-ricerca").click(function(){
         ricercaFilm();
+        ricercaSerieTv();
     });
 
     $(".campo-ricerca-input").keypress(function(event){
         if(event.which == 13) {
             ricercaFilm();
+            ricercaSerieTv();
         }
 
     });
@@ -37,14 +41,23 @@ $(document).ready(function() {
                     var lingua = film[i].original_language;
                     var voto = film[i].vote_average;
                     var stella = Math.round(voto / 2);
-                    var htmlStella = "";
-                    for (var j = 0; j < 5; j++) {
-                        if (j < stella ) {
-                            htmlStella += '<i class="fas fa-star"></i>'
-                        }else{
-                            htmlStella += '<i class="far fa-star"></i>'
-                        }
-                    }
+                    var htmlStellaVuota =
+                    '<i class="far fa-star"></i>'.repeat(5 - stella);
+                    var htmlStellaPiena =
+                    '<i class="fas fa-star"></i>'.repeat(stella);
+
+// Creazione delle stelle con il ciclo FOR di seguito
+                    // for (var j = 0; j < 5; j++) {
+                    //     if (j < stella ) {
+                    //         htmlStella += '<i class="fas fa-star"></i>'
+                    //     }else{
+                    //         htmlStella += '<i class="far fa-star"></i>'
+                    //     }
+                    // }
+                    // '<i class="fas fa-star"></i>'.repeat(stella);
+                    // '<i class="far fa-star"></i>'.repeat(5 - stella);
+// -------------------------------------------------------
+
                     var imgBandiera = bandiere(lingua);
 
                     var variabili = {
@@ -52,7 +65,7 @@ $(document).ready(function() {
                         titoloOriginale : titoloOriginale,
                         lingua : imgBandiera,
                         voto : voto,
-                        stella : htmlStella
+                        stella : htmlStellaPiena + htmlStellaVuota
                     };
 
 
@@ -67,6 +80,56 @@ $(document).ready(function() {
             }
         });
     };
+
+    function ricercaSerieTv() {
+        var ricercaSerie = $(".campo-ricerca-input").val();
+        $(".container").text(" ");
+        $.ajax({
+            url :"https://api.themoviedb.org/3/search/tv",
+            data : {
+                api_key : "7058fdc9b87af5cb2dbc92bd22e4e43c",
+                query : ricercaSerie.toLowerCase()
+            },
+            method : "get",
+            success : function (data){
+                var serie = data.results;
+                for (var k = 0; k < serie.length; k++) {
+                    // console.log(serie[i]);
+
+                    // creo delle variabili per prendere le informazioni di cui ho bisogno dai vari serie
+                    var titolo = serie[k].name;
+                    var titoloOriginale = serie[k].original_name;
+                    var lingua = serie[k].original_language;
+                    var voto = serie[k].vote_average;
+                    var stella = Math.round(voto / 2);
+                    var htmlStellaVuota =
+                    '<i class="far fa-star"></i>'.repeat(5 - stella);
+                    var htmlStellaPiena =
+                    '<i class="fas fa-star"></i>'.repeat(stella);
+
+                    var imgBandiera = bandiere(lingua);
+
+                    var variabiliSerie = {
+                        titoloSerie : titolo,
+                        titoloOriginaleSerie : titoloOriginale,
+                        linguaSerie : imgBandiera,
+                        votoSerie : voto,
+                        stellaSerie : htmlStellaPiena + htmlStellaVuota
+                    };
+
+
+                    var htmlFinaleSerie = templateFunctionSerie(variabiliSerie);
+                    // lo appendo nel contenitore dei dischi
+                    $(".container").append(htmlFinaleSerie);
+
+                };
+            },
+            error : function (){
+                alert("errore")
+            }
+        });
+    };
+
 
 function bandiere(lang) {
     if (lang == "en") {
