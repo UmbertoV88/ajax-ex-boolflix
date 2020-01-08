931f48fdfb91bb9dee1e79a287fec1bb18df60da
UmbertoV <umberto.ottone@gmail.com>
2020-01-08 22:01:13 +0100

    Se non c'è il risultato viene visualizzato un avviso

--- main.js
+++ main.js
@@ -26,65 +26,69 @@ $(document).ready(function() {
     //------------------------------------
     function ricercaFilm() {
         $(".testata").show();
-        var ricercaFilm = $(".campo-ricerca-input").val();
-        $(".container").text(" ");
+        var ricerca = $(".campo-ricerca-input").val();
+        $(".movie").hide();
         $.ajax({
             url : api_url_base + "/search/movie",
             data : {
                 api_key : api_key,
-                query : ricercaFilm.toLowerCase()
+                query : ricerca.toLowerCase()
             },
             method : "get",
             success : function (data){
-                var film = data.results;
-                for (var i = 0; i < film.length; i++) {
-                    // console.log(film[i]);
+                if (data.total_results > 0 ) {
+                    var film = data.results;
+                    for (var i = 0; i < film.length; i++) {
+                        // console.log(film[i]);
 
-                    // creo delle variabili per prendere le informazioni di cui ho bisogno dai vari film
-                    var titolo = film[i].title;
-                    var titoloOriginale = film[i].original_title;
-                    var lingua = film[i].original_language;
-                    var voto = film[i].vote_average;
-                    if(film[i].poster_path != null) {
-                        var immagine_locandina = image_url_base + image_size + film[i].poster_path;
-                    }else{
-                        var immagine_locandina = 'https://s3-ap-southeast-1.amazonaws.com/silverscreen-photos/1534489151m000001.jpg'
-                    };
+                        // creo delle variabili per prendere le informazioni di cui ho bisogno dai vari film
+                        var titolo = film[i].title;
+                        var titoloOriginale = film[i].original_title;
+                        var lingua = film[i].original_language;
+                        var voto = film[i].vote_average;
+                        if(film[i].poster_path != null) {
+                            var immagine_locandina = image_url_base + image_size + film[i].poster_path;
+                        }else{
+                            var immagine_locandina = 'https://s3-ap-southeast-1.amazonaws.com/silverscreen-photos/1534489151m000001.jpg'
+                        };
 
-                    var stella = Math.round(voto / 2);
-                    var htmlStellaVuota =
-                    '<i class="far fa-star"></i>'.repeat(5 - stella);
-                    var htmlStellaPiena =
-                    '<i class="fas fa-star"></i>'.repeat(stella);
+                        var stella = Math.round(voto / 2);
+                        var htmlStellaVuota =
+                        '<i class="far fa-star"></i>'.repeat(5 - stella);
+                        var htmlStellaPiena =
+                        '<i class="fas fa-star"></i>'.repeat(stella);
 
-// Creazione delle stelle con il ciclo FOR di seguito
-                    // for (var j = 0; j < 5; j++) {
-                    //     if (j < stella ) {
-                    //         htmlStella += '<i class="fas fa-star"></i>'
-                    //     }else{
-                    //         htmlStella += '<i class="far fa-star"></i>'
-                    //     }
-                    // }
-                    // '<i class="fas fa-star"></i>'.repeat(stella);
-                    // '<i class="far fa-star"></i>'.repeat(5 - stella);
-// -------------------------------------------------------
+    // Creazione delle stelle con il ciclo FOR di seguito
+                        // for (var j = 0; j < 5; j++) {
+                        //     if (j < stella ) {
+                        //         htmlStella += '<i class="fas fa-star"></i>'
+                        //     }else{
+                        //         htmlStella += '<i class="far fa-star"></i>'
+                        //     }
+                        // }
+                        // '<i class="fas fa-star"></i>'.repeat(stella);
+                        // '<i class="far fa-star"></i>'.repeat(5 - stella);
+    // -------------------------------------------------------
 
-                    var imgBandiera = bandiere(lingua);
+                        var imgBandiera = bandiere(lingua);
 
-                    var variabili = {
-                        titolo : titolo,
-                        titoloOriginale : titoloOriginale,
-                        lingua : imgBandiera,
-                        voto : voto,
-                        stella : htmlStellaPiena + htmlStellaVuota,
-                        image : immagine_locandina
-                    };
+                        var variabili = {
+                            titolo : titolo,
+                            titoloOriginale : titoloOriginale,
+                            lingua : imgBandiera,
+                            voto : voto,
+                            stella : htmlStellaPiena + htmlStellaVuota,
+                            image : immagine_locandina
+                        };
 
 
-                    var htmlFinale = templateFunction(variabili);
-                    // lo appendo nel contenitore dei dischi
-                    $(".container_film ").append(htmlFinale);
+                        var htmlFinale = templateFunction(variabili);
+                        // lo appendo nel contenitore dei dischi
+                        $(".container_film ").append(htmlFinale);
 
+                    }
+                }else{
+                    $(".container_film ").append(' Nessun film trovato con la query "'  + ricerca + '"');
                 };
             },
             error : function (){
@@ -94,54 +98,59 @@ $(document).ready(function() {
     };
 
     function ricercaSerieTv() {
-        var ricercaSerie = $(".campo-ricerca-input").val();
-        $(".container").text(" ");
+        $(".testata").show();
+        var ricerca = $(".campo-ricerca-input").val();
+        $(".serietv").hide();
         $.ajax({
             url :api_url_base + "/search/tv",
             data : {
                 api_key : api_key,
-                query : ricercaSerie.toLowerCase()
+                query : ricerca.toLowerCase()
             },
             method : "get",
             success : function (data){
-                var film = data.results;
-                for (var i = 0; i < film.length; i++) {
-                    // console.log(film[i]);
+                if (data.total_results > 0 ) {
+                    var film = data.results;
+                    for (var i = 0; i < film.length; i++) {
+                        // console.log(film[i]);
 
-                    // creo delle variabili per prendere le informazioni di cui ho bisogno dai vari film
-                    var titolo = film[i].name;
-                    var titoloOriginale = film[i].original_name;
-                    var lingua = film[i].original_language;
-                    var voto = film[i].vote_average;
+                        // creo delle variabili per prendere le informazioni di cui ho bisogno dai vari film
+                        var titolo = film[i].name;
+                        var titoloOriginale = film[i].original_name;
+                        var lingua = film[i].original_language;
+                        var voto = film[i].vote_average;
 
-                    if(film[i].poster_path != null) {
-                        var immagine_locandina = image_url_base + image_size + film[i].poster_path;
-                    }else{
-                        var immagine_locandina = 'https://s3-ap-southeast-1.amazonaws.com/silverscreen-photos/1534489151m000001.jpg'
-                    };
-                    
-                    var stella = Math.round(voto / 2);
-                    var htmlStellaVuota =
-                    '<i class="far fa-star"></i>'.repeat(5 - stella);
-                    var htmlStellaPiena =
-                    '<i class="fas fa-star"></i>'.repeat(stella);
+                        if(film[i].poster_path != null) {
+                            var immagine_locandina = image_url_base + image_size + film[i].poster_path;
+                        }else{
+                            var immagine_locandina = 'https://s3-ap-southeast-1.amazonaws.com/silverscreen-photos/1534489151m000001.jpg'
+                        };
 
-                    var imgBandiera = bandiere(lingua);
+                        var stella = Math.round(voto / 2);
+                        var htmlStellaVuota =
+                        '<i class="far fa-star"></i>'.repeat(5 - stella);
+                        var htmlStellaPiena =
+                        '<i class="fas fa-star"></i>'.repeat(stella);
 
-                    var variabili = {
-                        titolo : titolo,
-                        titoloOriginale : titoloOriginale,
-                        lingua : imgBandiera,
-                        voto : voto,
-                        stella : htmlStellaPiena + htmlStellaVuota,
-                        image : immagine_locandina
-                    };
+                        var imgBandiera = bandiere(lingua);
+
+                        var variabili = {
+                            titolo : titolo,
+                            titoloOriginale : titoloOriginale,
+                            lingua : imgBandiera,
+                            voto : voto,
+                            stella : htmlStellaPiena + htmlStellaVuota,
+                            image : immagine_locandina
+                        };
 
 
-                    var htmlFinale = templateFunction(variabili);
-                    // lo appendo nel contenitore dei dischi
-                    $(".container_serie").append(htmlFinale);
+                        var htmlFinale = templateFunction(variabili);
+                        // lo appendo nel contenitore dei dischi
+                        $(".container_serie").append(htmlFinale);
 
+                    }
+                }else{
+                    $(".container_serie ").append(' Nessuna serie trovata con la query "'  + ricerca + '"');
                 };
             },
             error : function (){
