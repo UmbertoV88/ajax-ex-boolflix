db1e9f2b76d3b0bf7d79cd35157ca3ec27d693fb
UmbertoV <umberto.ottone@gmail.com>
2020-01-08 16:18:58 +0100

    Correzioni funzioni e pulizia codice HTMl

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
 
     var templateHtml = $("#template").html();
     var templateFunction = Handlebars.compile(templateHtml);
-    var templateHtmlSerie = $("#templateSerietv").html();
-    var templateFunctionSerie = Handlebars.compile(templateHtmlSerie);
 
     $(".bottone-ricerca").click(function(){
         ricercaFilm();
@@ -21,6 +19,7 @@ $(document).ready(function() {
     //-------------FUNZIONI---------------
     //------------------------------------
     function ricercaFilm() {
+        $(".testata").show();
         var ricercaFilm = $(".campo-ricerca-input").val();
         $(".container").text(" ");
         $.ajax({
@@ -71,7 +70,7 @@ $(document).ready(function() {
 
                     var htmlFinale = templateFunction(variabili);
                     // lo appendo nel contenitore dei dischi
-                    $(".container").append(htmlFinale);
+                    $(".container_film ").append(htmlFinale);
 
                 };
             },
@@ -92,15 +91,15 @@ $(document).ready(function() {
             },
             method : "get",
             success : function (data){
-                var serie = data.results;
-                for (var k = 0; k < serie.length; k++) {
-                    // console.log(serie[i]);
+                var film = data.results;
+                for (var i = 0; i < film.length; i++) {
+                    // console.log(film[i]);
 
-                    // creo delle variabili per prendere le informazioni di cui ho bisogno dai vari serie
-                    var titolo = serie[k].name;
-                    var titoloOriginale = serie[k].original_name;
-                    var lingua = serie[k].original_language;
-                    var voto = serie[k].vote_average;
+                    // creo delle variabili per prendere le informazioni di cui ho bisogno dai vari film
+                    var titolo = film[i].name;
+                    var titoloOriginale = film[i].original_name;
+                    var lingua = film[i].original_language;
+                    var voto = film[i].vote_average;
                     var stella = Math.round(voto / 2);
                     var htmlStellaVuota =
                     '<i class="far fa-star"></i>'.repeat(5 - stella);
@@ -109,18 +108,18 @@ $(document).ready(function() {
 
                     var imgBandiera = bandiere(lingua);
 
-                    var variabiliSerie = {
-                        titoloSerie : titolo,
-                        titoloOriginaleSerie : titoloOriginale,
-                        linguaSerie : imgBandiera,
-                        votoSerie : voto,
-                        stellaSerie : htmlStellaPiena + htmlStellaVuota
+                    var variabili = {
+                        titolo : titolo,
+                        titoloOriginale : titoloOriginale,
+                        lingua : imgBandiera,
+                        voto : voto,
+                        stella : htmlStellaPiena + htmlStellaVuota
                     };
 
 
-                    var htmlFinaleSerie = templateFunctionSerie(variabiliSerie);
+                    var htmlFinale = templateFunction(variabili);
                     // lo appendo nel contenitore dei dischi
-                    $(".container").append(htmlFinaleSerie);
+                    $(".container_serie").append(htmlFinale);
 
                 };
             },
@@ -150,35 +149,3 @@ function bandiere(lang) {
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*Milestone 2:
-Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
-Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
-
-Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
-Qui un esempio di chiamata per le serie tv:
-https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=s
-crubs*/
